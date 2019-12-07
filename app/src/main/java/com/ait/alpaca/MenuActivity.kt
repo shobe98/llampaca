3c97dc12da4412c195f992e2a601decc0847f51d
Maxine Perroni-Scharf <maxineAPS@googlemail.com>
2019-12-07 22:31:03 +0100

    first UI stuff, menu, splash, challenge

--- app/src/main/java/com/ait/alpaca/MenuActivity.kt
+++ app/src/main/java/com/ait/alpaca/MenuActivity.kt
@@ -3,13 +3,17 @@ package com.ait.alpaca
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_menu.*
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class MenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        Glide.with(this).asGif().load(R.drawable.clouds).into(ivClouds)
 
         btnAlbum.setOnClickListener {
             startActivity(Intent(this@MenuActivity, ScrollingActivity::class.java))
--- app/src/main/java/com/ait/alpaca/SplashActivity.kt
+++ app/src/main/java/com/ait/alpaca/SplashActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_splash.*
 
 
 class SplashActivity : AppCompatActivity() {
@@ -12,10 +14,14 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        Glide.with(this).asGif().load(R.drawable.splash).into(ivSplash)
+
+
+
         Handler().postDelayed({
-            startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+            startActivity(Intent(this@SplashActivity, MenuActivity::class.java))
             finish()
-        }, 3000)
+        }, 4000)
     }
 }
 
