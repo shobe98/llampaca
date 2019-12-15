7bebd16db484c7f3f47df80ec37c5da6377b7caf
Maxine Perroni-Scharf <maxineAPS@googlemail.com>
2019-12-15 21:19:36 +0100

    added booleans with final alpacas

--- app/src/main/java/com/ait/alpaca/InstructionsActivity.kt
+++ app/src/main/java/com/ait/alpaca/InstructionsActivity.kt
@@ -2,11 +2,14 @@ package com.ait.alpaca
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.ait.alpaca.utils.ProgressUtils
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_challenge.*
 import kotlinx.android.synthetic.main.activity_challenge.ivClouds
 import kotlinx.android.synthetic.main.activity_instructions.*
 import kotlinx.android.synthetic.main.activity_menu.*
+import kotlinx.android.synthetic.main.activity_scrolling.*
 
 class InstructionsActivity : AppCompatActivity() {
 
@@ -16,7 +19,9 @@ class InstructionsActivity : AppCompatActivity() {
 
         Glide.with(this).asGif().load(R.drawable.clouds).into(ivCloudsInstructions)
 
-
+        if (ProgressUtils.isFinished()) {
+            ivAlpsInstructions.visibility = View.VISIBLE
+        }
 
 
         btnQuitInstructions.setOnClickListener {
--- app/src/main/java/com/ait/alpaca/SuccessActivity.kt
+++ app/src/main/java/com/ait/alpaca/SuccessActivity.kt
@@ -3,11 +3,13 @@ package com.ait.alpaca
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.ait.alpaca.data.Alpaca
 import com.ait.alpaca.utils.ProgressUtils
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_instructions.*
 import kotlinx.android.synthetic.main.activity_menu.*
+import kotlinx.android.synthetic.main.activity_scrolling.*
 import kotlinx.android.synthetic.main.activity_success.*
 
 class SuccessActivity : AppCompatActivity() {
@@ -21,6 +23,10 @@ class SuccessActivity : AppCompatActivity() {
         setContentView(R.layout.activity_success)
 
         Glide.with(this).asGif().load(R.drawable.clouds).into(ivCloudsSuccess)
+
+        if (ProgressUtils.isFinished()) {
+            ivAlpsSuccess.visibility = View.VISIBLE
+        }
 
         btnQuitSuccess.setOnClickListener {
             finish()
