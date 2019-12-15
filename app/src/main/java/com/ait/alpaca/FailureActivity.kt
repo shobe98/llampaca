9407b272632f53f4542fba2058395231a1ee38fa
Andrei <shobe195@gmail.com>
2019-12-15 20:35:41 +0100

    glides

--- app/src/main/java/com/ait/alpaca/FailureActivity.kt
+++ app/src/main/java/com/ait/alpaca/FailureActivity.kt
@@ -3,13 +3,18 @@ package com.ait.alpaca
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_failure.*
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class FailureActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_failure)
+
+        Glide.with(this).asGif().load(R.drawable.clouds).into(ivClouds)
+
 
         btnBack.setOnClickListener {
             startActivity(Intent(this@FailureActivity, ChallengeActivity::class.java))
--- app/src/main/java/com/ait/alpaca/InstructionsActivity.kt
+++ app/src/main/java/com/ait/alpaca/InstructionsActivity.kt
@@ -2,14 +2,20 @@ package com.ait.alpaca
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_challenge.*
+import kotlinx.android.synthetic.main.activity_challenge.ivClouds
 import kotlinx.android.synthetic.main.activity_instructions.*
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class InstructionsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_instructions)
+
+        Glide.with(this).asGif().load(R.drawable.clouds).into(ivClouds)
+
 
         btnQuitInstructions.setOnClickListener {
             finish()
